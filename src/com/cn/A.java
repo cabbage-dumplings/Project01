758d4e7a76259e197fb30e0b6ae98b678e13c1af
cabbage-dumplings <384201386@qq.com>
2019-06-30 09:45:05 +0800

    Initial commit

---
+++ src/com/cn/A.java
@@ -0,0 +1,14 @@
+package com.cn;
+
+/**
+ * @author WangKang
+ * @create 2019-06-28-14:54
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+        System.out.println();
+        int a = 1;
+        System.out.println(a);
+    }
+}
